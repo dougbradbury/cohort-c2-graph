c85ae62c1f9d8347b768dc48cfc484aac58eafc9
dougbradbury <doug@8thlight.com>
2015-10-30 16:42:59 -0500

    finish method extraction

--- src/c2/core.clj
+++ src/c2/core.clj
@@ -7,7 +7,7 @@
 (def f (clojure.java.io/file "data"))
 (def fs (file-seq f))
 
-(defn extract-links
+(defn extract-links []
   (with-open [output (clojure.java.io/writer "out/simple.txt" :append true)]
     (doseq [file fs]
       (if-not (.isDirectory file)
@@ -21,7 +21,7 @@
   (let [[src target] (clojure.string/split line #" ")]
     (edgewise/add-edge g src target)))
 
-(defn build-graph
+(defn build-graph []
   (with-open [rdr (reader "out/simple.txt")]
     (converter/->tgf (reduce graph-it (edgewise/empty-graph) (line-seq rdr)) "out/graph.tgf")))
 
